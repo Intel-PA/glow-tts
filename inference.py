19dbb8a63531d2566b7aa8094b5c214eb2bd8593
Kavi <jayathunge.work@gmail.com>
2021-04-09 11:29:11 +0100

    add code to switch between using gpu and cpu for inference

--- inference.py
+++ inference.py
@@ -17,7 +17,16 @@ import modules
 import models
 import utils
 
+GPU = True
 
+
+device = torch.device('cuda' if GPU else 'cpu')
+print('Using device:', device)
+if device.type == 'cuda':
+    print(torch.cuda.get_device_name(0))
+    print('Memory Usage:')
+    print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+    print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB')
 
 def get_output_filename(prefix, index):
   return f"./generated_wavs/{prefix}_speech_{index}.wav"
@@ -27,7 +36,7 @@ def get_output_filename(prefix, index):
 waveglow_path = '../glow_tts_pretrained_models/waveglow_256channels_ljs_v3.pt' # or change to the latest version of the pretrained WaveGlow.
 waveglow = torch.load(waveglow_path)['model']
 waveglow = waveglow.remove_weightnorm(waveglow)
-_ = waveglow.cuda().eval()
+_ = waveglow.to(device).eval()
 
 # If you are using your own trained model
 model_dir = "./logs/augmented_model/"
@@ -35,13 +44,13 @@ hps = utils.get_hparams_from_dir(model_dir)
 checkpoint_path = utils.latest_checkpoint_path(model_dir)
 
 # If you are using a provided pretrained model
-#hps = utils.get_hparams_from_file("./configs/base.json")
-#checkpoint_path = "../glow_tts_pretrained_models/glow_tts_pretrained.pt"
+# hps = utils.get_hparams_from_file("./configs/base.json")
+# checkpoint_path = "../glow_tts_pretrained_models/glow_tts_pretrained.pt"
 
 model = models.FlowGenerator(
     len(symbols) + getattr(hps.data, "add_blank", False),
     out_channels=hps.data.n_mel_channels,
-    **hps.model).to("cuda")
+    **hps.model).to(device)
 
 utils.load_checkpoint(checkpoint_path, model)
 model.decoder.store_inverse() # do not calcuate jacobians for fast decoding
@@ -67,8 +76,8 @@ for index, tst_stn in enumerate(strings):
         text_norm = text_to_sequence(tst_stn.strip(), ['english_cleaners'], cmu_dict)
     sequence = np.array(text_norm)[None, :]
     print("".join([symbols[c] if c < len(symbols) else "<BNK>" for c in sequence[0]]))
-    x_tst = torch.autograd.Variable(torch.from_numpy(sequence)).cuda().long()
-    x_tst_lengths = torch.tensor([x_tst.shape[1]]).cuda()
+    x_tst = torch.autograd.Variable(torch.from_numpy(sequence)).to(device).long()
+    x_tst_lengths = torch.tensor([x_tst.shape[1]]).to(device)
 
     with torch.no_grad():
         noise_scale = .667
