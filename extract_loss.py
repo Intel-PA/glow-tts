f626f857d6cef2a34344cc8df0095f4311f15aca
Kavi <jayathunge.work@gmail.com>
2021-06-04 10:47:39 +0100

    extract eval loss

--- extract_loss.py
+++ extract_loss.py
@@ -3,18 +3,20 @@ import sys
 
 EPOCH_EXP = r"(?<=Train Epoch: )\d+"
 LOSS_EXP = r"(?<=Loss: )\d+\.\d+"
+EVAL_EPOCH_EXP = r"(?<=Eval Epoch: )\d+"
 LR_EXP = r"(\d+\.\d+(e-\d+)?)(?=])"
 
 
 def get_losses(infile):
     losses = []
     lrates = []
+    eval_losses = [] 
     with open(infile, "r") as fh:
         line = fh.readline()
 
         while line:
-            if "Train Epoch:" in line:
-                try:
+            try:
+                if "Train Epoch:" in line:
                     epoch = re.search(EPOCH_EXP, line).group(0)
                     loss = re.search(LOSS_EXP, line).group(0)
                     losses.append((epoch, loss))
@@ -22,11 +24,18 @@ def get_losses(infile):
                     lr_line = fh.readline()
                     lr = re.search(LR_EXP, lr_line).group(0)
                     lrates.append((epoch, lr))
-                except Exception :
-                    print(line)
+
+                if "Eval Epoch:" in line:
+                    epoch = re.search(EVAL_EPOCH_EXP, line).group(0)
+                    loss = re.search(LOSS_EXP, line).group(0)
+                    eval_losses.append((epoch, loss))
+            
+            except Exception:
+                print(line)
+
             line = fh.readline()
 
-    return losses, lrates
+    return losses, lrates, eval_losses
 
 
 def prune_losses(losses):
@@ -43,6 +52,7 @@ def prune_losses(losses):
     return pruned_losses
 
 
+
 def save_to_csv(losses, filename):
     with open(filename, "w") as fh:
         for epoch, loss in losses:
@@ -53,8 +63,9 @@ def save_to_csv(losses, filename):
 if __name__ == "__main__":
     infile = sys.argv[1]
     outfile = sys.argv[2]
-    losses, lrates = get_losses(infile)
+    losses, lrates, eval_losses = get_losses(infile)
     losses = prune_losses(losses)
     lrates = prune_losses(lrates)
     save_to_csv(losses, f"{outfile}_losses.csv")
     save_to_csv(lrates, f"{outfile}_lrates.csv")
+    save_to_csv(eval_losses, f"{outfile}_eval_losses.csv")
