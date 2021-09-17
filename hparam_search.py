cff98bdeb013bee6aeeacf9cd1382702c4e8cb77
Kavi <jayathunge.work@gmail.com>
2021-09-17 15:56:45 +0100

    remove: n_trials param

--- hparam_search.py
+++ hparam_search.py
@@ -49,7 +49,7 @@ def start_search(gamma, aug_method, opt_config):
     gamma = gamma_to_str(gamma)
     project = f"glow-tts_{aug_method}_{gamma}"
     study = optuna.create_study(study_name=project, direction='minimize', pruner=optuna.pruners.HyperbandPruner())
-    study.optimize(lambda trial: objective(trial, gamma, aug_method, project, opt_config), n_startup_trials=15)
+    study.optimize(lambda trial: objective(trial, gamma, aug_method, project, opt_config))
 
 
 def objective(trial, gamma, aug_method, project, opt_config):
